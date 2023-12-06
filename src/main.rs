a157ba96a935cc2b57c36b0d4659222b92f96b77
Riccardo Mei <mei.riccardo96@gmail.com>
2023-12-06 13:47:36 +0100

    feat(main.rs): implement buffer reader and add active error handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,45 @@
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::PathBuf;
+
+use anyhow::Result;
 use clap::Parser;
 
-/// Search for a pattern in a file and display the lines that contain it.
+// Set custuom error handler
+#[derive(Debug)]
+struct CustomError(String);
+
+// Set cli parser
 #[derive(Parser)]
 struct Cli {
     pattern: String,
-    path: std::path::PathBuf,
+    file_path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
+    // Parse cli arguments
     let args = Cli::parse();
-    let content: String = std::fs::read_to_string(&args.path).expect("File not found.");
+    let file_path: PathBuf = args.file_path;
+    let pattern: String = args.pattern;
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line)
+    // Open file and create a buffer reader to avoid large in-memory reads
+    let file: File = match File::open(&file_path) {
+        Ok(file) => { file }
+        Err(error) => { panic!("\n! Error reading '{}'\n    {}\n", file_path.display(), error)}
+    };
+    let buf_reader: BufReader<File> = BufReader::new(file);
+
+    // Iterate lines and find matching pattern
+    println!("\nMATCHING LINES:");
+    for (line_number, reader_result) in buf_reader.lines().enumerate() {
+        let line: String = reader_result.unwrap();
+
+        if line.contains(&pattern) {
+            println!("({}) {}", line_number, line.trim());
         }
     }
+    println!();
+
+    Ok(())
 }
